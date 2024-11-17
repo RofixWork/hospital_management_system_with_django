831397d5f78f3d22b29d3d5d7c69383b8d82755c
Rofix <wourkout123@gmail.com>
2024-11-17 19:42:15 +0100

    Build Register and LOgin Templates

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+
 
 # Create your views here.
+class RegisterView(TemplateView):
+    template_name = "userauths/sign-up.html"
+
+
+class LoginView(TemplateView):
+    template_name = "userauths/sign-in.html"
